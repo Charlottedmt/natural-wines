c2097bf1fa3ff98bccc0bbe7df076a0555ae7f9b
Ryu Watanabe <rw.ryunosuke@gmail.com>
2021-03-23 14:58:27 -0700

    Locations & Bottles Controller generated

---
+++ app/controllers/bottles_controller.rb
@@ -0,0 +1,42 @@
+class BottlesController < ApplicationController
+  def index
+    @bottles = Bottle.all
+  end
+
+  def show
+    @bottle = Bottle.find(params[:id])
+  end
+
+  def new
+    @bottle = Bottle.new
+  end
+
+  def create
+    @bottle = Bottle.new(bottle_params)
+    if @bottle.save?
+      redirect_to bottles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @bottle = Bottle.find(params[:id])
+  end
+
+  def update
+    @bottle = Bottle.find(params[:id])
+    if @bottle.update(bottle_params)
+      redirect_to bottles_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def bottle_params
+    params.require(:bottle).permit(:address, :name, :category)
+  end
+
+end
---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,42 @@
+class LocationsController < ApplicationController
+
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save?
+      redirect_to locations_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+      redirect_to locations_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:address, :name, :category)
+  end
+end
