092bd870233b09d17dcb3ee20f3a110ed39f869e
Charlotte Demaret <demaret.cha@gmail.com>
2021-03-29 22:05:07 +0900

    working on adding the bottles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root to: 'locations#index'
+  root to: 'pages#home'
   resources :locations
+  resources :bottles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
