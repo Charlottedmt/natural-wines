e4cca655657a9f6c41d2279f37358f958a941795
Charlotte Demaret <demaret.cha@gmail.com>
2021-08-11 11:22:31 +0900

    changes on the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,28 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+    @locations = Location.all
+    @markers = @locations.geocoded.map do |location|
+      if location.category == "Shop"
+        {
+          lat: location.latitude,
+          lng: location.longitude,
+          image_url: helpers.asset_url('store.png'),
+          infoWindow: render_to_string(partial: "info_window", locals: { location: location })
+        }
+      else
+        {
+          lat: location.latitude,
+          lng: location.longitude,
+          image_url: helpers.asset_url('restaurant.png'),
+          # infoWindow: render_to_string(partial: "info_window", locals: { location: location })
+        }
+      end
+    end
+    @current_position =
+      {
+        image_url: helpers.asset_url('user_position.png')
+      }
   end
+
 end
